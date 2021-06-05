234b1feabc84ff5e1e15febe8e4e1b006e93a2e0
Michael Lan <michaellan202@gmail.com>
2021-06-05 12:24:00 -0700

    setup(): Add basic validation for options

--- lua/nnn.lua
+++ lua/nnn.lua
@@ -1,7 +1,19 @@
 local M = {}
 
+M.valid_options = {
+    'session',
+    'set_default_mappings',
+    'layout',
+    'action',
+    'session',
+    'command',
+}
+
 function M.setup(config)
     for k, v in pairs(config) do
+        if not vim.tbl_contains(M.valid_options, k) then
+            error("Invalid option to nnn setup(): " .. vim.inspect(k))
+        end
         vim.g['nnn#' .. k] = v
     end
 end
