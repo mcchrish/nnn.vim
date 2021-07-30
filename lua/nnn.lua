010cd7ed4aee144a4e81acd67c4ed1dc6a5f2f2a
Michael Chris Lopez <hello@michaelchris.space>
2021-07-30 13:18:16 +0800

    format lua using stylua

--- lua/nnn.lua
+++ lua/nnn.lua
@@ -1,23 +1,23 @@
 local M = {}
 
 M.valid_options = {
-    'set_default_mappings',
-    'layout',
-    'action',
-    'session',
-    'command',
-    'replace_netrw',
-    'statusline',
-    'shell',
+	"set_default_mappings",
+	"layout",
+	"action",
+	"session",
+	"command",
+	"replace_netrw",
+	"statusline",
+	"shell",
 }
 
 function M.setup(config)
-    for k, v in pairs(config) do
-        if not vim.tbl_contains(M.valid_options, k) then
-            error("Invalid option to nnn setup(): " .. vim.inspect(k))
-        end
-        vim.g['nnn#' .. k] = v
-    end
+	for k, v in pairs(config) do
+		if not vim.tbl_contains(M.valid_options, k) then
+			error("Invalid option to nnn setup(): " .. vim.inspect(k))
+		end
+		vim.g["nnn#" .. k] = v
+	end
 end
 
 return M
